17899f4bd216f82abab9b50f6b3ef3eafa05875e
bharath kristipati <bharath.kumar.kristipati@oracle.com>
2019-11-14 08:40:23 +0530

    added new test 2

--- src/test/java/com/slokam/automation/opencart/testscripts/TestVerifyAPI.java
+++ src/test/java/com/slokam/automation/opencart/testscripts/TestVerifyAPI.java
@@ -2,6 +2,7 @@ package com.slokam.automation.opencart.testscripts;
 
 import org.testng.annotations.Test;
 
+import com.slokam.automation.opencart.commons.pages.AdminLoginPage;
 import com.slokam.automation.opencart.testscripts.base.TestBase;
 
 public class TestVerifyAPI extends TestBase{
@@ -9,5 +10,12 @@ public class TestVerifyAPI extends TestBase{
 	@Test
 	public void test1() {
 		System.out.println("test1");
+	}
+	
+	
+	@Test
+	public void verifyAPIStatus() {
+		AdminLoginPage adminLoginPage = new AdminLoginPage(driver);
+		adminLoginPage.login("sdf", "sdfds");
 	}
 }
