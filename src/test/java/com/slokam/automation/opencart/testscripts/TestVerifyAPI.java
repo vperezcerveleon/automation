111a037838825e014d7d87d8422c59654526c391
bharath kristipati <bharath.kumar.kristipati@oracle.com>
2019-11-14 08:27:54 +0530

    I am adding new testcase

---
+++ src/test/java/com/slokam/automation/opencart/testscripts/TestVerifyAPI.java
@@ -0,0 +1,13 @@
+package com.slokam.automation.opencart.testscripts;
+
+import org.testng.annotations.Test;
+
+import com.slokam.automation.opencart.testscripts.base.TestBase;
+
+public class TestVerifyAPI extends TestBase{
+
+	@Test
+	public void test1() {
+		System.out.println("test1");
+	}
+}
